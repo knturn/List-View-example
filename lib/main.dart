fd2fa35aed8084b182d0ad1e395c5dd7e0a66d83
Kaan Turan <dyt.kaanturan@gmail.com>
2022-02-15 23:15:10 +0300

    Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:homework_1/view/Home/home_view.dart';
 
 void main() {
@@ -11,7 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeView(),
